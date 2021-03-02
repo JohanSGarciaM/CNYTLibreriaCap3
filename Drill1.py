1406b45cdbcf135157ec6841c3d3fce890e1fe9d
Sebastian Garcia <johan.garcia@mail.escuelaing.edu.co>
2021-03-02 18:32:37 -0500

    Programming Drill 1

---
+++ Drill1.py
@@ -0,0 +1,21 @@
+def marble(M,Istate,clicks):
+    FinalState=Istate
+    for i in range(clicks):
+        FinalState = multiplicacion(M,FinalState)
+    return FinalState
+
+
+def multiplicacion(M,state):
+    NextState = [0 for i in range(len(state))]
+    for i in range(len(state)):
+        for j in range(len(state)):
+            NextState[i]+=M[i][j]*state[j]
+    return NextState
+
+def main():
+    M = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,1,0,0,0,1],[0,0,0,1,0,0],[0,0,1,0,0,0],[1,0,0,0,1,0]]
+    Istate = [6,2,1,5,3,10]
+    clicks = int(input("please enter number of clicks"))
+    print(marble(M,Istate,clicks))
+
+main()
